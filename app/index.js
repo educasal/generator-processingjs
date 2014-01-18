a8cbf3c3d7d5c75ee119ddeba4d647c87f214d3a
t1st3 <contact@tiste.org>
2014-01-18 05:30:50 +0100

    introduce prompts

--- app/index.js
+++ app/index.js
@@ -22,27 +22,40 @@ ProcessingjsGenerator.prototype.askFor = function askFor() {
   // have Yeoman greet the user.
   console.log(this.yeoman);
 
-  var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
-  }];
+  var prompts = [
+		{
+			name: 'githubAccount',
+			message: 'What is your github account?'
+		},
+		{
+			name: 'projectName',
+			message: 'What is the name of your Processing project (the slug-name of the Github repository)?'
+		},
+		{
+			name: 'projectVersion',
+			message: 'What is the version of your Processing project?'
+		}
+	];
 
-  this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
-
-    cb();
-  }.bind(this));
+	this.prompt(prompts, function (props) {
+		this.githubAccount = props.githubAccount;
+		this.projectName = props.projectName;
+		this.projectVersion = props.projectVersion;
+		this.objectName = props.objectName;
+		cb();
+	}.bind(this));
 };
 
 ProcessingjsGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/templates');
+  this.mkdir('src');
+	this.template('src/_processing-project.js', 'src/' + this.projectName + '.js');
 
   this.copy('_package.json', 'package.json');
   this.copy('_bower.json', 'bower.json');
 	this.copy('bowerrc', '.bowerrc');
+	
+	this.mkdir('dist');
+	this.mkdir('doc');
 };
 
 ProcessingjsGenerator.prototype.projectfiles = function projectfiles() {
