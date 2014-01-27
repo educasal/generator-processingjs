14511543b769aca2e6c7fe052faebf5f881faf68
t1st3 <contact@tiste.org>
2014-01-27 03:08:32 +0100

    Add update-notifier and figlet on generator launch

--- app/index.js
+++ app/index.js
@@ -2,9 +2,10 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
+var figlet = require('figlet');
 
 
-var ProcessingjsGenerator = module.exports = function ProcessingjsGenerator(args, options, config) {
+var ProcessingjsGenerator = module.exports = function ProcessingjsGenerator(args, options) {
   yeoman.generators.Base.apply(this, arguments);
 
   this.on('end', function () {
@@ -19,25 +20,44 @@ util.inherits(ProcessingjsGenerator, yeoman.generators.Base);
 ProcessingjsGenerator.prototype.askFor = function askFor() {
   var cb = this.async();
 
-  // have Yeoman greet the user.
-  console.log(this.yeoman);
+  var t = this;
+	figlet('yo processingjs', function (err, data) {
+		if (err) {
+			console.log('Something went wrong with figlet');
+			console.dir(err);
+			return;
+		} else {
+			console.log(data);
+			var updateNotifier = require('update-notifier');
+			var notifier = updateNotifier(
+				{
+					packagePath: '../package.json',
+					packageName: 'generator-processingjs'
+				}
+			);
+			if (notifier.update) {
+				notifier.notify();
+			}
+			console.log(t.yeoman);
 
-  var prompts = [
-		{
-			name: 'githubAccount',
-			message: 'What is your github account?'
-		},
-		{
-			name: 'projectName',
-			message: 'What is the name of your Processing JS project (the slug-name of the Github repository)?'
+			var prompts = [
+				{
+					name: 'githubAccount',
+					message: 'What is your github account?'
+				},
+				{
+					name: 'projectName',
+					message: 'What is the name of your Processing JS project (the slug-name of the Github repository)?'
+				}
+			];
+
+			t.prompt(prompts, function (props) {
+				t.githubAccount = props.githubAccount;
+				t.projectName = props.projectName;
+				cb();
+			}.bind(t));
 		}
-	];
-
-	this.prompt(prompts, function (props) {
-		this.githubAccount = props.githubAccount;
-		this.projectName = props.projectName;
-		cb();
-	}.bind(this));
+	});
 };
 
 ProcessingjsGenerator.prototype.app = function app() {
